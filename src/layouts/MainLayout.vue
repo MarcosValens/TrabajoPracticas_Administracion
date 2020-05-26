7400c37f93c0b2ce89f3ba2ff1e88d725c678cb6
mmonteirocl-torre <miguelmonteiroclaveri@gmail.com>
2020-05-26 11:27:49 +0200

    Añadimos un par de cambios en el proyecto de administracion del liceu

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -11,11 +11,18 @@
           @click.capture="drawerClick"
           @click="drawer = !drawer"
         />
-        <q-avatar>
-          <q-img src="~/assets/esliceu-logo.png" alt="Logo de el liceu"/>
-        </q-avatar>
+
         <q-toolbar-title>Administració</q-toolbar-title>
 
+        <q-btn icon="far fa-user" flat round>
+          <q-menu>
+            <q-card>
+              <q-card-section>
+                {{}}
+              </q-card-section>
+            </q-card>
+          </q-menu>
+        </q-btn>
       </q-toolbar>
     </q-header>
 
@@ -27,9 +34,9 @@
       :content-class="$q.screen.height>489?'bg-grey-2 column justify-between':'bg-grey-2'"
     >
       <q-list>
-        <q-item clickable v-ripple v-for="link in links" :key="link.title" :to="link.link" >
+        <q-item clickable v-ripple v-for="link in links" :key="link.title" :to="link.link">
           <q-item-section avatar>
-            <q-icon :name="link.icon"/>
+            <q-icon :name="link.icon" />
           </q-item-section>
           <q-item-section>
             <q-item-label lines="1" style="font-size: 1.2em">{{link.title}}</q-item-label>
@@ -37,44 +44,48 @@
         </q-item>
       </q-list>
       <div class="q-mini-drawer-hide absolute" style="top: 15px; right: -17px">
-        <q-btn
-          dense
-          round
-          unelevated
-          color=primary
-          icon="chevron_left"
-          @click="miniState = true"
-        ></q-btn>
+        <q-btn dense round unelevated color="primary" icon="chevron_left" @click="miniState = true"></q-btn>
       </div>
 
       <div class="q-pa-md">
-        <q-separator class="q-mb-md"/>
+        <q-separator class="q-mb-md" />
         <div class="flex justify-end">
-          <q-btn dense color="secondary" outline icon-right="exit_to_app" label="Desconectarse" @click="disconnect"/>
+          <q-btn
+            dense
+            color="secondary"
+            outline
+            icon-right="exit_to_app"
+            label="Desconectarse"
+            @click="disconnect"
+          />
         </div>
       </div>
 
       <template v-slot:mini>
-        <q-list class="">
+        <q-list class>
           <q-item clickable v-ripple v-for="link in links" :key="link.title" :to="link.link">
             <q-item-section avatar>
-              <q-icon :name="link.icon"/>
+              <q-icon :name="link.icon" />
             </q-item-section>
-            <q-tooltip :delay="150"
-                       transition-hide="slide-left" anchor="center right" self="center left">
-              {{link.title}}
-            </q-tooltip>
+            <q-tooltip
+              :delay="150"
+              transition-hide="slide-left"
+              anchor="center right"
+              self="center left"
+            >{{link.title}}</q-tooltip>
           </q-item>
         </q-list>
 
         <div>
-          <q-separator class="q-mb-md"/>
+          <q-separator class="q-mb-md" />
           <div class="flex flex-center q-mb-md">
             <q-btn dense color="secondary" outline icon-right="exit_to_app" @click="disconnect">
               <q-tooltip
-                transition-hide="slide-left" content-class="bg-secondary" anchor="center right" self="center left">
-                Desconectarse
-              </q-tooltip>
+                transition-hide="slide-left"
+                content-class="bg-secondary"
+                anchor="center right"
+                self="center left"
+              >Desconectarse</q-tooltip>
             </q-btn>
           </div>
         </div>
@@ -82,60 +93,68 @@
     </q-drawer>
 
     <q-page-container>
-      <router-view/>
+      <router-view />
     </q-page-container>
   </q-layout>
 </template>
 
 <script>
+export default {
+  name: "MainLayout",
 
-  export default {
-    name: "MainLayout",
+  components: {},
 
-    components: {},
-
-
-    data() {
-      return {
-        links: [
-          {
-            title: "Panell d'administració",
-            icon: "fas fa-user-shield",
-            link: "/admin/panel",
-          },
-          {
-            title: "Usuaris",
-            icon: "account_circle",
-            link: "/admin/usuaris",
-          },
-          {
-            title: "Professors",
-            icon: "people",
-            link: "/admin/professors",
-          },
-        ],
-        drawer: false,
-        miniState: false,
-        roles: []
-      };
-    },
-
-    methods: {
-      drawerClick(e) {
-        if (this.miniState) {
-          this.miniState = false;
-          e.stopPropagation();
+  data() {
+    return {
+      links: [
+        {
+          title: "Panell d'administració",
+          icon: "fas fa-user-shield",
+          link: "/admin/panel"
+        },
+        {
+          title: "Usuaris",
+          icon: "account_circle",
+          link: "/admin/usuaris"
+        },
+        {
+          title: "Professors",
+          icon: "people",
+          link: "/admin/professors"
         }
-      },
-      disconnect() {
-        localStorage.removeItem("access_token")
-        localStorage.removeItem("refresh_token")
-        localStorage.removeItem("rol")
-        this.$router.push("/login")
+      ],
+      drawer: false,
+      miniState: false,
+      roles: [],
+      loguedUser: {
+        nom: "",
+        cognoms: "",
+        email: ""
       }
+    };
+  },
+
+  methods: {
+    drawerClick(e) {
+      if (this.miniState) {
+        this.miniState = false;
+        e.stopPropagation();
+      }
+    },
+    disconnect() {
+      localStorage.removeItem("access_token");
+      localStorage.removeItem("refresh_token");
+      localStorage.removeItem("rol");
+      this.$router.push("/login");
     }
-  };
+  },
+  async created() {
+    /**
+     * Cogemos la informacion propia del usuario logueado
+     */
+    const response = await this.$axiosCore.get("/private/usuario/me");
+  }
+};
 </script>
 <style>
-
 </style>>
